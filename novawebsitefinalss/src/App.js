549f9e8f5765292aeb0be4529b8f335ddcc685a0
rishiiiiiiishah <142864064+rishiiiiiiishah@users.noreply.github.com>
2024-02-19 16:25:50 +0530

    Add files via upload

---
+++ novawebsitefinalss/src/App.js
@@ -0,0 +1,16 @@
+import "./component/Navbar"
+import Navbar from "./component/Navbar";
+import FAQS from "./FAQs";
+
+function App() {
+  return (
+    <div>
+ <Navbar/>
+
+    <FAQS/>
+    </div>
+  
+  );
+}
+
+export default App;
